4f4853e065484fae61c9ccb288f74f83f8aaf90b
hubertsobolewski1337-hue <hubert.sobolewski1337@gmail.com>
2026-08-23 22:51:33 +0200

    Dodaj zweryfikowane bezpośrednie linki Allegro Lokalnie

---
+++ links-extra.js
@@ -0,0 +1,12 @@
+Object.assign(window.ST24_DATA.directUrls, {
+  5: 'https://allegrolokalnie.pl/oferta/oryginalna-bateria-toshiba-pa3536u1brs-pabas100-108v',
+  6: 'https://allegrolokalnie.pl/oferta/oryginalna-bateria-asus-a32k55-108v-4400mah-47wh',
+  11: 'https://allegrolokalnie.pl/oferta/karta-wifi-bluetooth-ralink-rt3290le-hp-690020001-mini-pcie',
+  12: 'https://allegrolokalnie.pl/oferta/karta-wifi-atheros-ar5bxb63-toshiba-wll3141d4-mini-pcie-54-mbs',
+  14: 'https://allegrolokalnie.pl/oferta/procesor-intel-core-i32370m-240-ghz-tyt',
+  18: 'https://allegrolokalnie.pl/oferta/procesor-intel-pentium-dualcore-t2130-186-ghz',
+  22: 'https://allegrolokalnie.pl/oferta/karta-wifi-ralink-rt5390-150-mbs-mini-pcie-asus',
+  23: 'https://allegrolokalnie.pl/oferta/karta-sieciowa-wifi-5-bluetooth-50-realtek-rtl8822ce-hp-m2',
+  24: 'https://allegrolokalnie.pl/oferta/karta-wifi-intel-300-mbs-245-ghz-lenovo',
+  27: 'https://allegrolokalnie.pl/oferta/dysk-ssd-128-gb-tgf'
+});
